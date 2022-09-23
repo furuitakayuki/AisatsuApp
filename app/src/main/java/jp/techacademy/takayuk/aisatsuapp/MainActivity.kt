3413cbce95f601892662bdf37b7da3b204abcb23
furuitakayuki <maki1904@icloud.com>
2022-09-23 17:31:38 +0900

    あいさつアプリ

--- app/src/main/java/jp/techacademy/takayuk/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/aisatsuapp/MainActivity.kt
@@ -1,11 +1,42 @@
 package jp.techacademy.takayuk.aisatsuapp
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
+            showTimePickerDialog()
+        }
+    }
+
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+
+                if (hour in 2..9 && minute >= 0) {
+                    textView.setText("おはようございます")
+                } else if(hour in 10..17 && minute >= 0) {
+                    textView.setText("こんにちわ")
+                } else if (hour in 18..23 && minute >= 0) {
+                    textView.setText("こんばんは")
+                } else
+                    textView.setText("こんばんは")
+
+            },
+            12, 0, true)
+        timePickerDialog.show()
     }
 }
